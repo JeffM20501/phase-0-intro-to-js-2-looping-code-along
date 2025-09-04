0e1dbf6753174c2bfffa52f83fbe416b6bbafca8
JeffM20501 <jeff.muna@student.moringaschool.com>
2025-09-04 13:05:15 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const names=['Charlie', 'Samip', 'Ali']
+
+const writeCards=(arr, string)=>{
+    let newArr = []
+    for(let i=0;i<arr.length;i++){
+        newArr.push(`Thank you, ${arr[i]}, for the wonderful ${string} gift!`)
+    }
+    return newArr
+}
+
+const countDown=(num)=>{
+    while(num>=0){
+        console.log(num--)
+    }
+}
+
+const count2=(num)=>{
+    for(let i=0;i<num;num--){
+        console.log(num)
+    }
+}
+
+count2(10)
+// countDown(10)
